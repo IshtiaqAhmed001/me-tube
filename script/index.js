636b0b3166d9a20e9590de45af12a92ba4a7362d
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-03-14 22:39:25 +1100

    added video cards

--- script/index.js
+++ script/index.js
@@ -7,6 +7,13 @@ fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
 .then(data=>displayCategories(data.categories));
 
 }
+
+function loadVideos(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res=>res.json())
+    .then(data=>displayVideos(data.videos))
+}
+
 function displayCategories(categories){
 // get the container 
 const categoryContainer = document.getElementById('category-container');
@@ -23,4 +30,33 @@ for(let item of categories){
 
 }
 
+const displayVideos =(videos)=>{
+const videoContainer = document.getElementById('video-container');
+
+videos.forEach(video=>{
+    // console.log(video)
+const videoCard = document.createElement('div');
+
+videoCard.innerHTML=`
+<div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src='${video.thumbnail}' />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+`
+videoContainer.append(videoCard);
+
+})
+
+}
+
 loadCategories()
+loadVideos()
